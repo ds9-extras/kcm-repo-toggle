186cf7c9a88c495aed8fe322afe1014c7f5b8755
Dan Leinir Turthra Jensen <admin@leinir.dk>
2017-02-28 11:35:37 +0000

    Add generic-use channel support, and UI for the duplicate error

--- src/Module.cpp
+++ src/Module.cpp
@@ -99,6 +99,9 @@ void Module::Private::populateSources()
 
     QMap<QString, QString> sldEntries;
     QString sldDir(backend->config()->findDirectory("Dir::Etc::sourceparts", QLatin1String("/etc/apt/sources.list.d/")));
+    while(sldDir.endsWith("/")) {
+        sldDir = sldDir.left(sldDir.length() - 1);
+    }
     QDir sld(sldDir);
     if(sld.exists()) {
         for(auto const &entry : sld.entryList(QDir::Files)) {
@@ -113,46 +116,53 @@ void Module::Private::populateSources()
     OSRelease os;
     QStringList paths = QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation);
     for(QString const& path : paths) {
-        QString channelsPath = QString("%1/release-channels/channels/%2").arg(path).arg(os.name);
-        QDir channels(channelsPath);
-        if(channels.exists()) {
-            for(QString const& channel : channels.entryList(QDir::Files)) {
-                QCheckBox *checkbox = new QCheckBox(channel);
-                QFile file(QString("%1/%2").arg(channelsPath).arg(channel));
-                QString contents;
-                if(file.open(QIODevice::ReadOnly)) {
-                    contents = file.readAll();
-                    file.close();
-                }
-                QStringList lines = contents.split("\n");
-                // Is the first line a comment? Use that as the title
-                if(lines[0].startsWith("#")) {
-                    checkbox->setText(lines[0].mid(1).trimmed());
-                }
-                // How about the second line? That'll be our description
-                if(lines.length() > 1 && lines[1].startsWith("#")) {
-                    checkbox->setToolTip(lines[1].mid(1).trimmed());
-                }
-                checkbox->setProperty("currentState", Qt::Unchecked);
-                // if file exists in /etc/apt/sources.lists.d/...
-                if(sldEntries.contains(channel)) {
-                    // and is identical to our file, check box...
-                    if(contents == sldEntries[channel]) {
-                        checkbox->setCheckState(Qt::Checked);
-                        checkbox->setProperty("currentState", Qt::Checked);
+        QStringList channelTypes = QStringList() << QString("%1/release-channels/channels/general-use").arg(path) << QString("%1/release-channels/channels/%2").arg(path).arg(os.name);
+        for(QString const &channelsPath : channelTypes) {
+            QDir channels(channelsPath);
+            if(channels.exists()) {
+                for(QString const& channel : channels.entryList(QDir::Files)) {
+                    QCheckBox *checkbox = new QCheckBox(channel);
+                    QFile file(QString("%1/%2").arg(channelsPath).arg(channel));
+                    QString contents;
+                    if(file.open(QIODevice::ReadOnly)) {
+                        contents = file.readAll();
+                        file.close();
                     }
-                    // and is different from our file, disable, describe error
-                    // nb: this also ensures we can handle two channels with the same .lists filename... just don't
-                    // enable the one that would otherwise replace what is already there. Needs saying in the UI somehow.
-                    else {
-                        checkbox->setEnabled(false);
-                        checkbox->setToolTip(i18nc("Checkbox tool tip which shows when the contents differ between the channel's .lists file and the .lists file with the same name in apt's soources.lists.d", "This entry cannot be enabled, as a channel with this filename already exists, but the contents differ."));
+                    QStringList lines = contents.split("\n");
+                    // Is the first line a comment? Use that as the title
+                    if(lines[0].startsWith("#")) {
+                        checkbox->setText(lines[0].mid(1).trimmed());
                     }
+                    // How about the second line? That'll be our description
+                    if(lines.length() > 1 && lines[1].startsWith("#")) {
+                        checkbox->setToolTip(lines[1].mid(1).trimmed());
+                    }
+                    checkbox->setProperty("currentState", Qt::Unchecked);
+                    q->ui->verticalLayout->addWidget(checkbox);
+                    // if file exists in /etc/apt/sources.lists.d/...
+                    if(sldEntries.contains(channel)) {
+                        // and is identical to our file, check box...
+                        if(contents == sldEntries[channel]) {
+                            checkbox->setCheckState(Qt::Checked);
+                            checkbox->setProperty("currentState", Qt::Checked);
+                        }
+                        // and is different from our file, disable, describe error
+                        // nb: this also ensures we can handle two channels with the same .lists filename... just don't
+                        // enable the one that would otherwise replace what is already there. Needs saying in the UI somehow.
+                        else {
+                            checkbox->setEnabled(false);
+                            checkbox->setToolTip(i18nc("Checkbox tool tip which shows when the contents differ between the channel's .lists file and the .lists file with the same name in apt's soources.lists.d", "This entry cannot be enabled, as a channel with this filename already exists, but the contents differ."));
+                            QLabel *label = new QLabel(QString("The contents of the files %1 and %2/%3 differ - you will have to manually remove %2/%3 to be able to select this channel.").arg(file.fileName()).arg(sldDir).arg(channel)); // this is a placeholder, hence no i18n...
+                            label->setWordWrap(true);
+                            q->ui->verticalLayout->addWidget(label);
+                            // TODO Make it possible to see the difference between the two files, and manually delete the
+                            // other (if it's not represented by another channel...) - this will need thorough thought.
+                        }
+                    }
+                    checkbox->setProperty("channelFile", channel);
+                    checkbox->setProperty("channelsDir", channelsPath);
+                    q->connect(checkbox, &QCheckBox::stateChanged, [this](){checkCheckStates();});
                 }
-                q->ui->verticalLayout->addWidget(checkbox);
-                checkbox->setProperty("channelFile", channel);
-                checkbox->setProperty("channelsDir", channelsPath);
-                q->connect(checkbox, &QCheckBox::stateChanged, [this](){checkCheckStates();});
             }
         }
     }
